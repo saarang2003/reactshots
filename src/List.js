98059a3894ebc7df94ee269d58050411173e00a1
saarang2003 <saranggp2018@gmail.com>
2025-10-04 16:34:27 +0530

    completed dependencies and setup folder

---
+++ src/List.js
@@ -0,0 +1,11 @@
+
+const List  = [
+    {   
+        id :  1,
+        title : "Project One",
+        desc : "This is the first project",
+        link : "</FirstProject>" 
+    }
+]
+
+export default List;
