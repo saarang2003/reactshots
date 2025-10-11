c6e19911f2daa79af64dafdb8f2d3202f375f97b
saarang2003 <saranggp2018@gmail.com>
2025-10-11 08:17:48 +0530

    did some ui updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,9 @@ import { Link } from "react-router-dom";
 import List from "./List";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import counterImage from './assets/counter.png';
+import dropdownImage from './assets/dropdown.png';
+import inputEditImage from './assets/input.png';
 
 function Home() {
   return (
@@ -49,12 +52,14 @@ function Home() {
 
       <div className="relative z-10 max-w-[586px] h-fit-content mx-auto mt-6 text-center px-2 ">
         <div>
+          <Link to ="/projects">
           <Button
             size="lg"
             className="bg-[#5417D7] hover:bg-[#3e0fb5] cursor-pointer  text-white px-6 py-3 rounded-lg shadow-2xl transition duration-300"
           >
             Explore Projects <ArrowRight />
           </Button>
+          </Link>
         </div>
       </div>
 
@@ -63,7 +68,7 @@ function Home() {
         <hr />
       </div>
 
-      <div className="relative gap-4 max-w-[1280px]  p-6 rounded-2xl shadow-2xl mx-auto mt-4">
+      <div className="relative gap-4 max-w-[1280px]  p-6 rounded-2xl shadow-2xl mx-auto mt-4 ">
         <h1 className="text-4xl text-[#d9e0ff] font-bold mb-1 ">React Projects</h1>
         <p className="text-[#4a4e5e]">
           Explore a variety of projects built with React and enhance your
@@ -73,12 +78,18 @@ function Home() {
         {/* Projects */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-2">
 
-          <Card className="mt-6 w-[350px] h-fit-content bg-red-200 shadow-2xl">
-            <CardTitle className="text-center font-bold text-2xl">Counter App</CardTitle>
+          <Card className="mt-6 w-[350px] h-full flex flex-col 
+  rounded-xl 
+  shadow-2xl shadow-white/10 
+  border border-white/5 
+  bg-gradient-to-r from-[#585ac4]/40 to-[#393ce3]/40 
+  backdrop-blur-2xl
+  ">
+            <CardTitle className="text-center font-bold text-2xl text-accent/75 ">Counter App</CardTitle>
             <div className="w-[80%] mx-auto h-48 bg-gray-300 rounded-lg flex items-center justify-center">
-              Image hai i
+              <img src={counterImage} alt="counter app image"   className="h-[200px] w-[80%] object-contain rounded-lg" />
             </div>
-            <CardContent className="line-clamp-3 ">In this project, we built a counter application with Undo and Redo functionality using React. The app tracks state history, allowing users to reverse or reapply actions showcasing the concept of time travel in state management.</CardContent>
+            <CardContent className="line-clamp-3 text-gray-100 ">In this project, we built a counter application with Undo and Redo functionality using React. The app tracks state history, allowing users to reverse or reapply actions showcasing the concept of time travel in state management.</CardContent>
             <div className="px-5 mb-2">
               <Link to = '/counter'>
               <Button   className="  bg-[#5417D7] hover:bg-[#3e0fb5] cursor-pointer text-white px-6 py-2 rounded-lg shadow-2xl transition duration-300">
@@ -90,13 +101,19 @@ function Home() {
 
 
     {/* 2nd one  */}
-          <Card className="mt-6 w-[350px] h-fit-content bg-red-200 shadow-2xl">
-            <CardTitle className="text-center font-bold text-2xl">Dropdown Component</CardTitle>
+          <Card className="mt-6 w-[350px] h-full flex flex-col 
+  rounded-xl 
+  shadow-2xl shadow-white/10 
+  border border-white/5 
+  bg-gradient-to-r from-[#585ac4]/40 to-[#393ce3]/40 
+  backdrop-blur-2xl
+  ">
+            <CardTitle className="text-center font-bold text-2xl text-accent/75">Dropdown Component</CardTitle>
             <div className="w-[80%] mx-auto h-48 bg-gray-300 rounded-lg flex items-center justify-center">
-              Image hai i
+              <img src={dropdownImage} alt="dropdown component image" className="h-[200px] w-[80%] object-contain rounded-lg" />
             </div>
-            <CardContent className="line-clamp-3 ">In this project, we built a dropdown component using React. The component allows users to select an option from a list, showcasing the use of controlled components and state management in React.</CardContent>
-          
+            <CardContent className="line-clamp-3 text-gray-100">In this project, we built a dropdown component using React. The component allows users to select an option from a list, showcasing the use of controlled components and state management in React.</CardContent>
+
             <div className="px-5 mb-2">
               <Link to = '/dropdown'>
               <Button   className="  bg-[#5417D7] hover:bg-[#3e0fb5] cursor-pointer text-white px-6 py-2 rounded-lg shadow-2xl transition duration-300">
@@ -107,12 +124,18 @@ function Home() {
           </Card>
 
          {/* 3rd one */}
-         <Card className="mt-6 w-[350px] h-fit-content bg-red-200 shadow-2xl">
-            <CardTitle className="text-center font-bold text-2xl">Editable Input Component</CardTitle>
+         <Card className="mt-6 w-[350px] h-full flex flex-col 
+  rounded-xl 
+  shadow-2xl shadow-white/10 
+  border border-white/5 
+  bg-gradient-to-r from-[#585ac4]/40 to-[#393ce3]/40 
+  backdrop-blur-2xl
+  ">
+            <CardTitle className="text-center font-bold text-2xl text-accent/75">Editable Input Component</CardTitle>
             <div className="w-[80%] mx-auto h-48 bg-gray-300 rounded-lg flex items-center justify-center">
-              Image hai i
+              <img src={inputEditImage} alt="editable input component image" className="h-[200px] w-[80%] object-contain rounded-lg" />
             </div>
-            <CardContent className="line-clamp-3 ">In this project, we built an editable input component using React. The component allows users to edit text inline, showcasing the use of controlled components and state management in React.</CardContent>
+            <CardContent className="line-clamp-3 text-gray-100">In this project, we built an editable input component using React. The component allows users to edit text inline, showcasing the use of controlled components and state management in React.</CardContent>
 
             <div className="px-5 mb-2">
               <Link to = '/inputedit'>
@@ -133,7 +156,7 @@ function Home() {
 
       </div>
           
-        <div className="flex relative z-10  justify-center  mt-6 mb-4">
+        <div className="flex relative z-10  justify-center  mt-12 mb-4">
           <Link to="/projects">
           <Button className="bg-[#5417D7] hover:bg-[#3e0fb5] cursor-pointer  text-white px-12 py-6 text-xl font-semibold rounded-lg shadow-2xl  transition duration-300">View All Projects
       <ArrowRight className="w-18 h-18"/>
